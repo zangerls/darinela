4f731cd369053101e3a3b60e0a0f75e3922a7d86
Simon Zangerl <simon.zangerl@hotmail.com>
2026-05-08 11:40:23 +0200

    Wired up first draft of entire landing page

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,31 @@
+import { About } from "@/components/about";
+import { MediaPlayer } from "@/components/media-player";
+import { Contact } from "@/components/contact";
+import { Education } from "@/components/education";
+import { Experience } from "@/components/experience";
+import { Gallery } from "@/components/gallery";
+import { Hero } from "@/components/hero";
+import { Navbar } from "@/components/navbar";
+import { Achievements } from "@/components/achievements";
+import { Performances } from "@/components/performances";
+import { UpcomingPerformances } from "@/components/upcoming-performances";
+
+export default function Page() {
+    return (
+        <div className="min-h-screen bg-background text-foreground selection:bg-black selection:text-white">
+            <Navbar />
+            <main className="relative z-10 bg-background shadow-2xl">
+                <Hero />
+                <About />
+                <MediaPlayer />
+                <Experience />
+                <Education />
+                <Achievements />
+                <Performances />
+                <UpcomingPerformances />
+                <Gallery />
+                <Contact />
+            </main>
+        </div>
+    );
+}
