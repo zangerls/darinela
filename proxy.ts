8fda2dd69a0bbd41423ebfbf2d8e6c18c298c4e6
Simon Zangerl <simon.zangerl@hotmail.com>
2026-05-14 18:29:19 +0200

    Removed password gate for project

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,10 @@
 import createMiddleware from "next-intl/middleware";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { routing } from "./i18n/routing";
-
-const PASSWORD = "darinela";
-const COOKIE = "auth";
 
 const intlMiddleware = createMiddleware(routing);
 
 export default function middleware(req: NextRequest) {
-    const { pathname } = req.nextUrl;
-
-    if (pathname === "/login") return NextResponse.next();
-
-    if (req.cookies.get(COOKIE)?.value !== PASSWORD) {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
-
     return intlMiddleware(req);
 }
 
