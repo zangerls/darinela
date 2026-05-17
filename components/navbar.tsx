fa078e9eeb34eb2087f16f61e26356e2b463ef6b
Simon Zangerl <simon.zangerl@hotmail.com>
2026-05-17 20:24:39 +0200

    Fixed navbar item alignment for multi line links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -225,7 +225,7 @@ export function Navbar() {
                                         data-clickable
                                         href={item.href}
                                         onClick={() => setIsOpen(false)}
-                                        className="group relative block text-4xl font-black tracking-tighter transition-all duration-300 sm:text-5xl md:text-6xl lg:text-7xl"
+                                        className="group relative block text-center text-4xl font-black tracking-tighter transition-all duration-300 sm:text-5xl md:text-6xl lg:text-7xl"
                                     >
                                         <span
                                             data-clickable
